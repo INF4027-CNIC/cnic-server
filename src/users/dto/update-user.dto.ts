eb5fb956c2c41ec411a86363c1b74d700f11bd46
Fenyep <0Uiimi1D5/-yYkGt>
2023-01-16 09:01:17 +0100

    feature/users: Possibiliry now to create, delete, and update a given user in the platform

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,4 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
 
-export class updateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {}
